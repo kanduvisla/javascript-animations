a8bae00e6b0bdf95d3cdbe33181aa77e2ed79437
Giel Berkers <info@gielberkers.com>
2016-01-10 19:23:58 +0100

    First line

---
+++ demo1.js
@@ -0,0 +1,15 @@
+// Simple path coordinates
+var line = [
+    {x:0, y:300},
+    {x:100, y:300},
+    {x:200, y:300},
+    {x:300, y:300},
+    {x:400, y:300},
+    {x:500, y:300},
+    {x:600, y:300},
+    {x:700, y:300},
+    {x:800, y:300}
+];
+
+// Draw the line:
+svgElement.drawPolygon(line);
---
+++ svg.js
@@ -0,0 +1,44 @@
+/**
+ * Helper for SVG drawing
+ * @param elementId
+ * @constructor
+ */
+function Svg(elementId)
+{
+    "use strict";
+
+    // Reference to the HTML element
+    var element = document.getElementById(elementId);
+
+    /**
+     * Get the HTML Element
+     * @returns {HTMLElement}
+     */
+    this.getElement = function()
+    {
+        return element;
+    };
+
+    /**
+     * Draw a line on the HTML Element
+     * @param {Array} coordinates
+     */
+    this.drawPolygon = function(coordinates)
+    {
+        var line = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
+        line.setAttribute('stroke', '#000000');
+        line.setAttribute('stroke-width', '5');
+        var points = [];
+        for (var index in coordinates) {
+            if (coordinates.hasOwnProperty(index)) {
+                var coordinate = coordinates[index];
+                points.push(coordinate.x + ',' + coordinate.y);
+            }
+        }
+        line.setAttribute('points', points.join(' '));
+        element.appendChild(line);
+        return line;
+    };
+}
+
+var svgElement = new Svg('svg');
